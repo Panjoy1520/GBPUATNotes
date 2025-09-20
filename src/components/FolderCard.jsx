059f6ca67196305117e115b41b94181731f7df86
harikeshpandey <harrypandey121@gmail.com>
2025-09-20 10:30:13 +0530

    Fixed issue #6 Cards are not overflowing Now

--- src/components/FolderCard.jsx
+++ src/components/FolderCard.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function FolderCard({ icon, title,link }) {
   return (
     <Link to={link}>
-    <div className="bg-neutral-700 hover:bg-neutral-600 p-6 rounded-2xl text-center h-45 cursor-pointer transition-all duration-300 shadow-md hover:shadow-lg transform hover:-translate-y-1">
+    <div className="bg-neutral-700 hover:bg-neutral-600 p-6 rounded-2xl text-center h-full cursor-pointer transition-all duration-300 shadow-md hover:shadow-lg transform hover:-translate-y-1">
       <div className="flex items-center justify-center w-16 h-16 mx-auto mb-4 rounded-full">
         <img src={icon} alt={title} className="w-15 h-15 object-contain" />
       </div>
